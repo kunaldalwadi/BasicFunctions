f68a6cf0eb860f2717fa0fc4b1bd877848c62cae
Kunal Dalwadi <kunaldalwadi@gmail.com>
2021-09-30 18:12:43 -0400

    Add files via upload

---
+++ FibonacciRecursion.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+
+public class FibonacciRecursion {
+
+    static HashMap<Integer, Integer> cache = new HashMap<Integer, Integer>();
+
+    public static void main(String[] args) {
+
+        int N = 50;
+
+        System.out.println(fibonacci1(N));
+        System.out.println(fibonacci2(N));
+
+    }
+
+    //Without memoization
+    public static int fibonacci1(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fibonacci1(n-1) + fibonacci1(n-2);
+        }
+    }
+
+    //With Memoization (saving expensive method calls to save multiple times computation time)
+    private static int fibonacci2(int n) {
+
+        if (cache.containsKey(n))
+        {
+            return cache.get(n);
+        }
+
+        int total;
+
+        if (n<2)
+        {
+            total = n;
+        }
+        else
+        {
+            total = fibonacci2(n-1) + fibonacci2(n-2);
+        }
+
+        cache.put(n,total);
+
+        return total;
+    }
+
+
+}
